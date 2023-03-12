c528b4259543b40f2ac622730ff0a6681bc77f79
Timothy Roy <timothy.roy@protonmail.ch>
2023-03-12 08:50:24 -0500

    Add GH template repo

--- github/create_repo/create_repo.tf
+++ github/create_repo/create_repo.tf
@@ -3,3 +3,11 @@ resource "github_repository" "terraform-scripts" {
   description = "Terraform scripts to manage cloud providers: GH, AWS, DO, etc"
   visibility  = "public"
 }
+
+resource "github_repository" "template-repo" {
+  name        = "my-template-repo"
+  description = "A template repo to initialize with a README"
+  visibility  = "public"
+  is_template = true
+  topics      = ["template", "terraform"]
+}
