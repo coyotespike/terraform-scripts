865401a7a5c0714e242cec1d5542f139ea0e9d76
Timothy Roy <timothy.roy@protonmail.ch>
2023-03-13 13:56:04 -0500

    Create convolutions repo

--- github/create_repo/create_repo.tf
+++ github/create_repo/create_repo.tf
@@ -11,3 +11,14 @@ resource "github_repository" "template-repo" {
   is_template = true
   topics      = ["template", "terraform"]
 }
+
+resource "github_repository" "convolutions-from-scratch" {
+  name        = "convolutions-from-scratch"
+  description = "Some models built from scratch with PyTorch during my graduate program at UT Austin"
+  visibility  = "public"
+  topics      = ["machine-learning", "convolutions", "cnn", "unet", "fcn", "pytorch"]
+  template {
+    owner      = "coyotespike"
+    repository = "my-template-repo"
+  }
+}
